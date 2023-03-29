b1fe0a7b75e0fa126660d4d8a7b497d9f529279a
Acevedo Eze <ezeelkpo2016@gmail.com>
2023-03-29 02:36:42 -0300

    Se retiro toda la logica de la Activity

--- app/src/main/java/com/lospibescompany/viewbinding/MainActivity.java
+++ app/src/main/java/com/lospibescompany/viewbinding/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     private MainActivityViewModel mv;
     private ActivityMainBinding binding;
     private Context context;
-    public String aux = "Dolar a Euro";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +27,13 @@ public class MainActivity extends AppCompatActivity {
         //Inicializar MainActivityViewModel mv              //referencia a la app        //Clase que quiero crear
         mv = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
 
-        binding.rbDolarEuro.setChecked(true);
-        binding.etDolaresCant.setEnabled(true);
-        binding.rbEuroDolar.setChecked(false);
-        binding.etEurosCant.setEnabled(false);
 
-        //Poner listener al boton Convertir//De parametro va objeto OnClickListener
-        binding.btConvertir.setOnClickListener(new View.OnClickListener() {
+
+        binding.btConvertir.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                //Devuelve un editable que se puede parsear a necesidad
-                if(aux == "Euro a Dolar"){
-                    mv.conversion(binding.etEurosCant.getText().toString(), aux);
-                    Toast.makeText(MainActivity.this, binding.etEurosCant.getText().toString() , Toast.LENGTH_LONG).show();
-                }
-                else if (aux == "Dolar a Euro"){
-                    mv.conversion(binding.etDolaresCant.getText().toString(), aux);
-                    Toast.makeText(MainActivity.this, binding.etDolaresCant.getText().toString() , Toast.LENGTH_SHORT).show();
-                }
 
+                mv.conversion(binding.etDolaresCant.getText().toString(), binding.etEurosCant.getText().toString());
             }
         });
 
@@ -58,32 +45,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        binding.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        binding.rbDolarEuro.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // Manejar cambio de selección
-                RadioButton radioButton = findViewById(checkedId);
+            public void onClick(View view) {
+                binding.etDolaresCant.setEnabled(true);
+                binding.etEurosCant.setEnabled(false);
+                binding.etEurosCant.setText("");
 
-                //Toast.makeText(context, aux, Toast.LENGTH_SHORT).show();
-                if(aux == "Euro a Dolar"){
-                    aux = "Dolar a Euro";
-                    binding.rbDolarEuro.setChecked(true);
-                    binding.etDolaresCant.setEnabled(true);
-                    binding.rbEuroDolar.setChecked(false);
-                    binding.etEurosCant.setEnabled(false);
-                    binding.etEurosCant.setText("");
-                    //Toast.makeText(context, "dolares deshabilitados", Toast.LENGTH_SHORT).show();
-                }
-                else if(aux == "Dolar a Euro"){
-                    aux = "Euro a Dolar";
-                    binding.rbDolarEuro.setChecked(false);
-                    binding.etDolaresCant.setEnabled(false);
-                    binding.rbEuroDolar.setChecked(true);
-                    binding.etEurosCant.setEnabled(true);
-                    binding.etDolaresCant.setText("");
-                    //Toast.makeText(context, "euros deshabilitados", Toast.LENGTH_SHORT).show();
-                }
-                //Toast.makeText(MainActivity.this, radioButton.getText() , Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.rbEuroDolar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.etDolaresCant.setEnabled(false);
+                binding.etEurosCant.setEnabled(true);
+                binding.etDolaresCant.setText("");
             }
         });
 
--- app/src/main/java/com/lospibescompany/viewbinding/MainActivityViewModel.java
+++ app/src/main/java/com/lospibescompany/viewbinding/MainActivityViewModel.java
@@ -2,6 +2,8 @@ package com.lospibescompany.viewbinding;
 
 import android.app.Application;
 import android.content.Context;
+import android.text.Editable;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -10,10 +12,12 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.text.DecimalFormat;
+
 //Clase padre de vistas en android
 public class MainActivityViewModel extends AndroidViewModel {
     private Context context;//Variable contexto para pasar al constructor de la clase
-    private MutableLiveData<Double> conversion = null;
+    private MutableLiveData<Double> resultado = null;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
@@ -26,24 +30,24 @@ public class MainActivityViewModel extends AndroidViewModel {
     //variable resultado
     public LiveData<Double> getConversion(){
         //Pregunto si el resultado es nulo
-        if(conversion == null){
-            this.conversion = new MutableLiveData<>();
+        if(resultado == null){
+            this.resultado = new MutableLiveData<>();
         }
-        return conversion;
+        return resultado;
     }
-
-    public void conversion(String nro, String conv){
-        double numero = Double.parseDouble(nro);
-        double dolar = 1.09;
-        double euro = 0.92;
-        Toast.makeText(context, nro, Toast.LENGTH_SHORT).show();
-        if(conv == "Dolar a Euro"){
-            this.conversion.setValue(numero/dolar);
-            Toast.makeText(context, "llego de dolar a euro", Toast.LENGTH_LONG).show();
+    public void conversion(String dolar, String euro){
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        if(dolar.length() == 0){
+            double nro2 = Double.parseDouble(euro);
+            double eur = 0.92;
+            double convertido = Double.parseDouble(decimalFormat.format(nro2/eur));
+            this.resultado.setValue(convertido);
         }
-        else if(conv == "Euro a Dolar") {
-            this.conversion.setValue(numero/euro);
-            Toast.makeText(context, "llego de euro a dolar", Toast.LENGTH_LONG).show();
+        else if(euro.length() == 0){
+            double nro1 = Double.parseDouble(dolar);
+            double dol = 1.08;
+            double convertido = Double.parseDouble(decimalFormat.format(nro1/dol));
+            this.resultado.setValue(convertido);
         }
     }
 }
